b090beeec283e397a29a8438c5259fef042d99de
BugHunter-X <askzenai@gmail.com>
2025-02-11 15:16:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Some code that uses Tailwind CSS classes
+div className="bg-gray-100 p-4 rounded-lg shadow-md">
+  <h2 className="text-xl font-bold">My Component</h2>
+  <p className="text-gray-700">This is some text.</p>
+</div>
+```
